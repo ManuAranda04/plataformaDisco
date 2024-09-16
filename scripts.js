ab6860671fe74f0be2b2db796be81505a41a18e0
Manuel Aranda <manuivan2004@gmail.com>
2024-09-16 15:15:06 -0300

    Agregado scripts de JS

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let menu = document.getElementById('menu');
+abrirMenu();
+
+function abrirMenu(){
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+}
