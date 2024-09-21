da31dd5e9cf4520bff679dbd5ece2aafe84fa912
Manuel Aranda <manuivan2004@gmail.com>
2024-09-21 12:22:09 -0300

    Terminada primera parte del proyecto

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
+/* Menu hamburguesa */
 let menu = document.getElementById('menu');
+
+/* Tours */
+function saludarTours(){
+    let nombre;
+    let edad;
+    let span = document.getElementById("welcome");
+
+    nombre = prompt("Cuál es tu nombre?").toUpperCase();
+    while (nombre.length < 3){
+        nombre = prompt("Ingrese un nombre valido(al menos 3 letras)").toUpperCase();
+    }
+    
+    span.textContent = `Hola, ${nombre}!`;
+}
 
 function abrirMenu(){
     if(menu.classList.contains('hidden')){
