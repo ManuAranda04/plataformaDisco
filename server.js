66b56728940ac6a74dcc4e7844ecc4bc3453661b
Manuel Aranda <manuivan2004@gmail.com>
2024-11-05 16:24:17 -0300

    Creado el servidor

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const express = require("express");
+const express = require("express")
+const mongoose = require('mongoose')
 const router = require('./routes/server')
+const Albumes = require('./models/Album')
+const Users = require('./models/User')
+const url = 'mongodb+srv://manuivan2004:pGnT5f2we0LyITto@plataformadisco.a5kht.mongodb.net/?retryWrites=true&w=majority&appName=plataformaDisco'
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use('/', router)
 
-app.listen(3000, () => {
-  console.log("Servidor escuchando en el puerto 3000");
-});
+const connectToMongo = async ()=>{
+  try {
+    await mongoose.connect(url)
+
+    app.listen(3000, () => {
+      console.log("Servidor escuchando en el puerto 3000 y DB conectado");
+    });
+  }catch (error) {
+    console.log(error)
+  }
+}
+
+connectToMongo()
