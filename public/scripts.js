3a34adf7e5d6c3a3449caf06ede5ce66900fc70f
Manuel Aranda <manuivan2004@gmail.com>
2024-11-07 22:39:53 -0300

    Borrada función de favoritos, movida a addAlbum.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,4 @@
-//Menu hamburguesa
 let menu = document.getElementById('menu');
-let favoriteButton = document.querySelectorAll('.favoriteButton')
 
 function abrirMenu(){
     if(menu.classList.contains('hidden')){
@@ -9,19 +7,3 @@ function abrirMenu(){
         menu.classList.add('hidden');
     }
 }
-
-//Favoritos
-favoriteButton.forEach(button => {
-    button.addEventListener('click', function(){
-        const icon = this.querySelector('.favoriteIcon');
-        if (icon.classList.contains('fa-regular')) {
-            icon.classList.remove('fa-regular');
-            icon.classList.add('fa-solid');
-            icon.style.color = '#FFD43B';
-        } else {
-            icon.classList.remove('fa-solid');
-            icon.classList.add('fa-regular');
-            icon.style.color = '#fff';
-        }
-    })    
-});
